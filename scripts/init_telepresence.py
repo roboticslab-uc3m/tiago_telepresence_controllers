2d472b78cffb7d6ff386f342ccef86e64180fc3f
Bartek Łukawski <bwmn.peter@gmail.com>
2024-09-11 18:02:54 +0200

    Tuck arm in init script

--- scripts/init_telepresence.py
+++ scripts/init_telepresence.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 
 import rospy
+import actionlib
+from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
+from sensor_msgs.msg import JointState
 from controller_manager_msgs.srv import SwitchController
 
 if __name__ == "__main__":
   rospy.init_node("init_telepresence")
+  rospy.loginfo("Waiting for play_motion...")
+  client = actionlib.SimpleActionClient("play_motion", PlayMotionAction)
+  client.wait_for_server()
+  rospy.loginfo("...connected.")
+
+  rospy.wait_for_message("joint_states", JointState)
+  rospy.sleep(3.0)
+
+  rospy.loginfo("Tuck arm...")
+  goal = PlayMotionGoal()
+  goal.motion_name = 'home'
+  goal.skip_planning = False
+
+  client.send_goal(goal)
+  client.wait_for_result(rospy.Duration(10.0))
+  rospy.loginfo("Arm tucked.")
 
   rospy.wait_for_service("controller_manager/switch_controller")
   manager = rospy.ServiceProxy("controller_manager/switch_controller", SwitchController)
