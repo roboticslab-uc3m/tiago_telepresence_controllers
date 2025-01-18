cbbe7bfeb540b6040c9558b46c155eaadcfa2a2f
Bartek Łukawski <bwmn.peter@gmail.com>
2025-01-18 19:32:01 +0100

    Define ST problem using PK steps

--- src/st/chainiksolverpos_st.cpp
+++ src/st/chainiksolverpos_st.cpp
@@ -4,13 +4,34 @@
 
 #include <vector>
 
+#include "TiagoSubproblems.hpp"
+
 using namespace roboticslab;
 
 namespace
 {
     ScrewTheoryIkProblem * buildProblem(const PoeExpression & poe)
     {
-        return nullptr; // TODO
+        const auto & exp1 = poe.exponentialAtJoint(0);
+        const auto & exp2 = poe.exponentialAtJoint(1);
+        const auto & exp3 = poe.exponentialAtJoint(2);
+        const auto & exp4 = poe.exponentialAtJoint(3);
+        const auto & exp5 = poe.exponentialAtJoint(4);
+        const auto & exp6 = poe.exponentialAtJoint(5);
+        const auto & exp7 = poe.exponentialAtJoint(6);
+
+        const auto o = exp1.getOrigin() + exp1.getAxis(); // point on axis 1, but not on axis 7
+        const auto & b = exp2.getOrigin(); // intersection of axes 2 and 3
+        const auto & d = exp6.getOrigin(); // intersection of axes 6 and 7
+
+        ScrewTheoryIkProblem::Steps steps;
+        steps.emplace_back(std::vector<int>{0}, new PadenKahanOne(exp1, d));
+        steps.emplace_back(std::vector<int>{3}, new PadenKahanThree(exp4, d, b));
+        steps.emplace_back(std::vector<int>{1, 2}, new PadenKahanTwoNormal(exp2, exp3, d, b));
+        steps.emplace_back(std::vector<int>{4, 5}, new PadenKahanTwoNormal(exp5, exp6, b, d));
+        steps.emplace_back(std::vector<int>{6}, new PadenKahanOne(exp7, o));
+
+        return ScrewTheoryIkProblem::create(poe, steps);
     }
 }
 
